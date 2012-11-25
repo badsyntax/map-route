d9ba5068872c2eab2f5cf6c4d1655a335e0333d4
Richard Willis <willis.rh@gmail.com>
2012-11-25 23:24:49 +0000

    Removed redundant events code

--- httpdocs/assets/js/App.js
+++ httpdocs/assets/js/App.js
@@ -28,30 +28,3 @@ App.inherits = function(_sub, _super) {
   _sub.prototype = new F();
   _sub.prototype.constructor = _sub;
 }; 
-
-App.Events = (function() {
-  return {
-    _handlers: {},
-    on: function(name, handler) {
-      if (!this._handlers[name]) {
-        this._handlers[name] = $.Callbacks();
-      }
-      this._handlers[name].add(handler);
-    },
-    off: function(name, handler) {
-      if (handler) {
-        return this._handlers[name].remove(handler);
-      }
-      this._handlers[name].empty();
-    },
-    trigger: function(name) {
-      var args = Array.prototype.slice.call(arguments, 1);
-      if ($.isFunction(name)) {
-        return name.apply(this, args);
-      }
-      if (this._handlers[name]) {
-        this._handlers[name].fireWith(this, args);
-      }
-    }
-  };
-}());
