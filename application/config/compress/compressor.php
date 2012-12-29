a28687702443e570e330d35a5d303acb20d61dab
Richard Willis <willis.rh@gmail.com>
2012-12-29 11:14:45 +0000

    Whitespace formatting

--- application/config/compress/compressor.php
+++ application/config/compress/compressor.php
@@ -1,9 +1,9 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 return array(
-  'yui'         => array(
-    'java'        => 'java',
-    'jar'       => 'vendor/yui/yuicompressor-2.4.6.jar',
+  'yui' => array(
+    'java'  => 'java',
+    'jar'   => 'vendor/yui/yuicompressor-2.4.6.jar',
   ),
   'uglifyjS' => array(
     'cmd' => 'uglifyjs'
