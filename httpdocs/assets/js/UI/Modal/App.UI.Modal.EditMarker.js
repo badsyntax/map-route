713102b5762db4e47faf2029cf1ba3ff974cfeea
Richard Willis <willis.rh@gmail.com>
2013-03-14 00:00:52 +0000

    Integrated fileupload into edit marker modal

--- httpdocs/assets/js/UI/Modal/App.UI.Modal.EditMarker.js
+++ httpdocs/assets/js/UI/Modal/App.UI.Modal.EditMarker.js
@@ -1,53 +1,89 @@
 App.UI.Modal.EditMarker = (function(base) {
 
-  var selector = '#modal-edit-marker';
-  var model;
+  var shown;
+  var container;
 
-  function onShown() {
+  var upload = {
+    init: function () {
+      this.uploaded = 0;
+      this.getElements();
+      this.initPlugin();
+    },
+    getElements: function() {
+      this.uploadCcontainer = $('#fileupload');
+      this.progressContainer = container.find('.fileupload-progress');
+      this.uploadedContainer = container.find('.fileupload-uploaded');
+    },
+    initPlugin: function() {
+      this.fileupload = this.uploadCcontainer.fileupload({
+        autoUpload: true,
+        dataType: 'json',
+        acceptFileTypes: /(\.|\/)(png|jpe?g)$/i,
+        type: 'POST',     
+        url: '/api/photos',
+        progressall: this.onProgressAll.bind(this),
+        start: this.onStart.bind(this),
+        stop: this.onStop.bind(this)
+      })
+      .on('fileuploaddone', this.onDone.bind(this))
+      .data('blueimp-fileupload');
+    },
+    updateUploadedMessage: function() {
+      var msg = [
+        this.uploaded,
+        (this.uploaded === 1) ? 'file' : 'files',
+        'uploaded'
+      ].join(' ');
+      this.uploadedContainer.html(msg);
+    },
+    onProgressAll: function(e, data) {
+      var progress = parseInt(data.loaded / data.total * 100, 10);
+      this.progressContainer
+        .find('.progress')
+        .attr('aria-valuenow', progress)
+        .find('.bar').css(
+          'width',
+          progress + '%'
+        );
+    },
+    onStart: function (e) {
+      this.fileupload._transition(this.progressContainer);
+    },
+    onStop: function (e) {
+      this.fileupload._transition(this.progressContainer);
+    },
+    onDone: function (e, data) {
+      this.uploaded++;
+      this.updateUploadedMessage();
+    }
+  };  
 
-    var container = $(this);
-    var field = container.find('#inputSearchLocation');
+  function initPlacesSearch() {
+    var search = new App.UI.PlacesSearch(container.find('#inputSearchLocation'));
+    search.on('onAddressSelect', function(e, result, status) {});
+  }
 
-    (new App.UI.PlacesSearch(field))
-      .on('onAddressSelect', function(e, result, status) {
-        var location = result.geometry.location;
-        model.latitude(location.lat());
-        model.longitude(location.lng());
-      });
-
+  function initTabs() {
     container.find('.nav-tabs a').on('click', function(e) {
       e.preventDefault();
       $(this).tab('show');
     });
+  }
 
-    var container = $('#fileupload');
-
-    console.log(container.length);
-
-    container.fileupload({
-            // Uncomment the following to send cross-domain cookies:
-            //xhrFields: {withCredentials: true},
-            autoUpload: true,
-            dataType: 'json',
-            multipart: true,
-            acceptFileTypes: /(\.|\/)(png|jpe?g)$/i,
-            type: 'POST',     
-            previewMaxWidth: 78,
-            previewMaxHeight: 53,
-            url: '/api/photos'
-        });
-
-    container.on('fileuploadcompleted', onFileUploadCompleted);
-
-    function onFileUploadCompleted(e, data) {
-      var completedFileRows = data.filesContainer.find('.template-download').not('.error');
-      completedFileRows.remove();
+  function onShown() {
+    if (shown) {
+      return;
     }
-
+    shown = true;
+    container = $(this);
+    initPlacesSearch();
+    initTabs();
+    upload.init();
   }
   
   function show(dataModel, actionCallback) {
-    base.show(selector, {
+    shown = false;
+    var config = {
       heading: 'Edit location',
       buttons: [{
         title: 'Save',
@@ -58,11 +94,11 @@ App.UI.Modal.EditMarker = (function(base) {
         action: '',
         type: ''
       }]
-    }, dataModel, onShown);
+    };
+    base.show('#modal-edit-marker', config, dataModel, onShown);
   }
 
   return $.extend({}, base, {
     show: show
   });
-
 }(App.UI.Modal));
